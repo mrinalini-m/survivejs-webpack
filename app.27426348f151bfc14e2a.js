54bd9cf1f91bee72f882368cba742c0ef10abe22
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2016-11-12 01:04:16 -0800

    Updates

--- app.27426348f151bfc14e2a.js
+++ app.27426348f151bfc14e2a.js
@@ -1,2 +1,2 @@
 webpackJsonp([0,3],[function(n,e,o){var t=o(1);o(2),document.body.appendChild(t())},function(n,e){n.exports=function(){var n=document.createElement("h1");return n.className="pure-button",n.innerHTML="Hello world",n}}]);
-//# sourceMappingURL=app.js.map
+//# sourceMappingURL=app.27426348f151bfc14e2a.js.map
