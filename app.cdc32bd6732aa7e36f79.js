289f48b311239fbefe878135527a4ea5fd770357
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2016-11-14 00:40:02 -0800

    Updates

--- app.cdc32bd6732aa7e36f79.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0,3],{0:function(n,e,o){o(1);var t=o(30);document.body.appendChild(t())},30:function(n,e){n.exports=function(){var n=document.createElement("h1");return n.className="pure-button",n.innerHTML="Hello world",n}}});
-//# sourceMappingURL=app.cdc32bd6732aa7e36f79.js.map
